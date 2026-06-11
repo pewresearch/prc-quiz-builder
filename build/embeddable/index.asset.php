dccb966f04ee611129ddd9d9835e4284ec81eb89
prcdevgitbot <webdev@pewresearch.org>
2026-06-11 20:59:28 +0000

    See https://github.com/pewresearch/prc-platform/commit/d7761d93242201ede791b863b2d8ae197e8ad3a2 from refs/tags/v1.8.25

--- build/embeddable/index.asset.php
+++ build/embeddable/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('prc-components', 'prc-icons', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives'), 'version' => '393c49301230d487cd86');
+<?php return array('dependencies' => array('prc-components', 'prc-icons', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives'), 'version' => 'e44675581b7842f34b20');
