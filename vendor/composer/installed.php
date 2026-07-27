f8a171ec427d66976c17c280f325c0a5830fb7e5
prcdevgitbot <webdev@pewresearch.org>
2026-07-27 15:54:07 +0000

    See https://github.com/pewresearch/prc-platform/commit/e11ddc6a6f965dc2fcc3bdf577744f175d99eee6 from refs/tags/v1.12.3

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'prc/quiz-builder',
         'pretty_version' => 'dev-trunk',
         'version' => 'dev-trunk',
-        'reference' => '57477e5cf4abd36f4cb9b6345434f268ca923b35',
+        'reference' => '60e0f63163ba422e2200566d7b7ee190c48d0453',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -247,7 +247,7 @@
         'prc/quiz-builder' => array(
             'pretty_version' => 'dev-trunk',
             'version' => 'dev-trunk',
-            'reference' => '57477e5cf4abd36f4cb9b6345434f268ca923b35',
+            'reference' => '60e0f63163ba422e2200566d7b7ee190c48d0453',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
